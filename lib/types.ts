df60d3a8f7ce37ac62bf3afd70b0cf0a5471601c
DaanGamesDG <daan@daangamesdg.xyz>
2022-06-02 22:56:53 +0200

    types: add more WithLanyard types

--- lib/types.ts
+++ lib/types.ts
@@ -1,8 +1,11 @@
+import type { NextPage } from "next";
 import type React from "react";
 import type { UseLanyardResults } from "./hooks/Lanyard/types";
 
 /* === Component Types === */
 export type FC<Props extends Record<string, any> = Record<string, never>> = React.FC<Props & { children?: React.ReactNode }>;
 export type FCWithLanyard<Props extends Record<string, any> = Record<string, never>> = React.FC<
-	Props & { children?: React.ReactNode; Lanyard: UseLanyardResults }
+	Props & { children?: React.ReactNode; lanyard: UseLanyardResults }
 >;
+
+export type NextPageWithLanyard<Props extends Record<string, any> = Record<string, never>> = NextPage<Props & { lanyard: UseLanyardResults }>;
