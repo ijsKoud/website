f2991ed1795231827136b3bcc57ef8cb07c2428f
DaanGamesDG <daan@daangamesdg.xyz>
2022-06-03 21:48:19 +0200

    feat(Navbar): add Discord status pop-up w/ animations

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
 import type { FCWithLanyard } from "../../lib/types";
 import Button from "../Button";
 import { Discord } from "../Discord";
-import ToolTip from "../Tooltip";
+import { motion, useAnimation, Variants } from "framer-motion";
 
 export const Navbar: FCWithLanyard = ({ lanyard }) => {
+	const controller = useAnimation();
+	const [discordVisible, setDiscordVisible] = useState(false);
+	const [stateTimeout, setStateTimeout] = useState<NodeJS.Timeout>();
+
+	const variants: Variants = {
+		hidden: {
+			pointerEvents: "none",
+			opacity: 0,
+			transform: "translateY(0px)",
+			transition: {
+				duration: 0.3,
+				ease: [0.6, 0, 0.17, 1]
+			}
+		},
+		visible: {
+			pointerEvents: "all",
+			opacity: 1,
+			transform: "translateY(10px)",
+			transition: {
+				duration: 0.3,
+				ease: [0.6, 0, 0.17, 1]
+			}
+		}
+	};
+
+	useEffect(() => {
+		if (discordVisible) {
+			if (stateTimeout) clearTimeout(stateTimeout);
+			void controller.start("visible");
+		} else {
+			const timeout = setTimeout(() => void controller.start("hidden"), 1e2);
+			setStateTimeout(timeout);
+		}
+
+		return () => {
+			controller.stop();
+			if (stateTimeout) clearTimeout(stateTimeout);
+		};
+	}, [discordVisible]);
+
 	return (
 		<div className="navbar-container-wrapper">
 			<div className="navbar-container">
@@ -13,15 +54,23 @@ export const Navbar: FCWithLanyard = ({ lanyard }) => {
 					<Button path="/stats" title="Stats" style="string" type="link" />
 				</div>
 				<div className="discord-component-container">
-					<ToolTip content="Click for more info!">
-						<button className="discord-profile-button">
-							{lanyard.status?.discord_status === "offline" ? "OFFLINE" : "ONLINE"}
-							<p className={lanyard.status?.discord_status === "offline" ? "offline" : "online"} />
-						</button>
-					</ToolTip>
-					<div className="discord-component">
+					<button
+						onMouseEnter={() => setDiscordVisible(true)}
+						onMouseLeave={() => setDiscordVisible(false)}
+						className="discord-profile-button"
+					>
+						{lanyard.status?.discord_status === "offline" ? "OFFLINE" : "ONLINE"}
+						<p className={lanyard.status?.discord_status === "offline" ? "offline" : "online"} />
+					</button>
+					<motion.div
+						className="discord-component"
+						onMouseEnter={() => setDiscordVisible(true)}
+						onMouseLeave={() => setDiscordVisible(false)}
+						animate={controller}
+						variants={variants}
+					>
 						<Discord lanyard={lanyard} />
-					</div>
+					</motion.div>
 				</div>
 			</div>
 		</div>
