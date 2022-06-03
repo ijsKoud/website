ae917c9401be7954f8e616b49af998505631b6c7
DaanGamesDG <daan@daangamesdg.xyz>
2022-06-03 17:16:54 +0200

    feat(Navbar): more crazy stuff

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -5,19 +5,25 @@ import ToolTip from "../Tooltip";
 
 export const Navbar: FCWithLanyard = ({ lanyard }) => {
 	return (
-		<div className="navbar-container">
-			<div>
-				<Button path="/" title="Home" style="string" type="link" />
-				<Button path="/about" title="About Me" style="string" type="link" />
-				<Button path="/stats" title="Stats" style="string" type="link" />
+		<div className="navbar-container-wrapper">
+			<div className="navbar-container">
+				<div>
+					<Button path="/" title="Home" style="string" type="link" />
+					<Button path="/about" title="About Me" style="string" type="link" />
+					<Button path="/stats" title="Stats" style="string" type="link" />
+				</div>
+				<div className="discord-component-container">
+					<ToolTip content="Click for more info!">
+						<button className="discord-profile-button">
+							{lanyard.status?.discord_status === "offline" ? "OFFLINE" : "ONLINE"}
+							<p className={lanyard.status?.discord_status === "offline" ? "offline" : "online"} />
+						</button>
+					</ToolTip>
+					<div className="discord-component">
+						<Discord lanyard={lanyard} />
+					</div>
+				</div>
 			</div>
-			<ToolTip content="Click for more info!">
-				<button className="discord-profile-button">
-					{lanyard.status?.discord_status === "offline" ? "OFFLINE" : "ONLINE"}
-					<p className={lanyard.status?.discord_status === "offline" ? "offline" : "online"} />
-				</button>
-			</ToolTip>
-			<Discord lanyard={lanyard} />
 		</div>
 	);
 };
