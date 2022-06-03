f3d7fbad36f8bb8268fb9fdbe96e15fc6e5d4ca7
DaanGamesDG <daan@daangamesdg.xyz>
2022-06-03 17:10:17 +0200

    feat(Navbar): add status button

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,15 +1,22 @@
 import type { FCWithLanyard } from "../../lib/types";
 import Button from "../Button";
 import { Discord } from "../Discord";
+import ToolTip from "../Tooltip";
 
 export const Navbar: FCWithLanyard = ({ lanyard }) => {
 	return (
-		<div>
+		<div className="navbar-container">
 			<div>
 				<Button path="/" title="Home" style="string" type="link" />
 				<Button path="/about" title="About Me" style="string" type="link" />
 				<Button path="/stats" title="Stats" style="string" type="link" />
 			</div>
+			<ToolTip content="Click for more info!">
+				<button className="discord-profile-button">
+					{lanyard.status?.discord_status === "offline" ? "OFFLINE" : "ONLINE"}
+					<p className={lanyard.status?.discord_status === "offline" ? "offline" : "online"} />
+				</button>
+			</ToolTip>
 			<Discord lanyard={lanyard} />
 		</div>
 	);
