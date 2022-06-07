94dc3d53f6eda28f63f639c30d39fb78c38cf511
DaanGamesDG <daan@daangamesdg.xyz>
2022-06-07 18:37:19 +0200

    feat: add wakatime stats chart

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -2,11 +2,13 @@ import Page from "../components/Page";
 import type { NextPageWithLanyard } from "../lib/types";
 import GitHubCalendar from "react-github-calendar";
 import PinnedRepos from "../components/PinnedRepos";
+import WakaTimeStats from "../components/WakaTimeStats";
 
 const Stats: NextPageWithLanyard = () => {
 	return (
 		<Page>
 			<PinnedRepos />
+			<WakaTimeStats />
 			<h1>Commit Graph</h1>
 			<GitHubCalendar username="DaanGamesDG" style={{ marginBottom: "2rem" }} />
 		</Page>
