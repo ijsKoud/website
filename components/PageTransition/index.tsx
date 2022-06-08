5f01144a36e4d2acaeb6fd5fad84b6ba0e02db33
DaanGamesDG <daan@daangamesdg.xyz>
2022-06-08 16:09:59 +0200

    feat(PageTransition): add smoother animation

--- components/PageTransition/index.tsx
+++ components/PageTransition/index.tsx
@@ -1,25 +1,35 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, Variants } from "framer-motion";
 import { useRouter } from "next/router";
 import React from "react";
 import type { FC } from "../../lib/types";
+
+const variants: Variants = {
+	initial: {
+		opacity: 0
+	},
+	animate: {
+		opacity: 1,
+		transition: {
+			duration: 0.3,
+			ease: [0.4, 0, 0.2, 1]
+		}
+	},
+	exit: {
+		opacity: 0,
+		transform: "translateX(1rem)",
+		transition: {
+			duration: 0.3,
+			ease: [0.4, 0, 0.2, 1]
+		}
+	}
+};
 
 const PageTransition: FC = ({ children }) => {
 	const { route } = useRouter();
 
 	return (
 		<AnimatePresence exitBeforeEnter>
-			<motion.div
-				key={route}
-				initial={{ opacity: 0 }}
-				animate={{
-					opacity: 1,
-					transition: {
-						duration: 0.5,
-						ease: [0.6, -0.05, 0.01, 0.99]
-					}
-				}}
-				exit={{ opacity: 0 }}
-			>
+			<motion.div key={route} variants={variants} initial="initial" animate="animate" exit="exit">
 				{children}
 			</motion.div>
 		</AnimatePresence>
