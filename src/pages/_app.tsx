b3188e0cc9896f5ef01f338cf693c486c3dca371
ijsKoud <github@ijskoud.dev>
2022-11-02 14:18:52 +0100

    chore: fresh start

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,43 +1,11 @@
-import "../styles/index.scss";
-import "tippy.js/dist/tippy.css";
-import "react-loading-skeleton/dist/skeleton.css";
-
+import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
-import { useLanyard } from "../lib/hooks/Lanyard/UseLanyard";
-import type { UseLanyardResults } from "../lib/hooks/Lanyard/types";
-import Head from "next/head";
-import config from "../../config.json";
-import Navbar from "../components/other/Navbar";
-import PageTransition from "../components/general/PageTransition";
-import Footer from "../components/other/Footer";
 
-const App = ({ Component, pageProps }: AppProps<{ lanyard: UseLanyardResults }>) => {
-	const lanyard = useLanyard({ userId: config.discord });
-
+const App = ({ Component, pageProps }: AppProps) => {
 	return (
-		<>
-			<Head>
-				<link rel="icon" type="image/x-icon" href="/favicon.ico" />
-				<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-				<link rel="manifest" href="/favicons/site.webmanifest" />
-				<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color={config.meta.theme_color} />
-				<link rel="shortcut icon" href="/favicons/favicon.ico" />
-				<meta name="msapplication-TileColor" content={config.meta.theme_color} />
-				<meta name="msapplication-config" content="/favicons/browserconfig.xml" />
-				<meta name="theme-color" content={config.meta.theme_color} />
-
-				<meta property="og:title" content={config.meta.title} />
-				<meta property="og:type" content="site" />
-				<meta property="og:description" content={config.meta.description} />
-			</Head>
-			<Navbar lanyard={lanyard} />
-			<PageTransition>
-				<Component {...pageProps} lanyard={lanyard} />
-			</PageTransition>
-			<Footer lanyard={lanyard} />
-		</>
+		<ThemeProvider enableSystem enableColorScheme attribute="class">
+			<Component {...pageProps} />
+		</ThemeProvider>
 	);
 };
 
