1c65a8cef3f3dd49c629011b5fdfbd0e4d590cc6
ijsKoud <github@ijskoud.dev>
2022-11-02 16:46:17 +0100

    chore: add theme styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,43 @@ module.exports = {
 	content: ["./app/**/*.{js,ts,jsx,tsx}", "./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
 	darkMode: "class",
 	theme: {
-		extend: {}
+		fontFamily: {
+			sans: ["Nunito", "sans-serif"]
+		},
+		fontSize: {
+			base: ["21px"],
+			lg: ["26.25px"],
+			xl: ["32.81px"],
+			"2xl": ["41.02px"],
+			"3xl": ["51.27px"],
+			"4xl": ["64.09px"]
+		},
+		backdropBlur: {
+			"bg-dark": "175px",
+			"bg-light": "175px"
+		},
+		colors: {
+			primary: "#4E9ED7",
+			background: "#131313",
+			"background-900": "rgba(19, 19, 19, 0.9)",
+			"background-800": "rgba(19, 19, 19, 0.8)",
+			"background-700": "rgba(19, 19, 19, 0.7)",
+			"background-600": "rgba(19, 19, 19, 0.6)",
+			"background-500": "rgba(19, 19, 19, 0.5)",
+			"background-400": "rgba(19, 19, 19, 0.4)",
+			"background-300": "rgba(19, 19, 19, 0.3)",
+			"background-200": "rgba(19, 19, 19, 0.2)",
+			"background-100": "rgba(19, 19, 19, 0.1)",
+			"white-900": "rgba(255, 255, 255, 0.9)",
+			"white-800": "rgba(255, 255, 255, 0.8)",
+			"white-700": "rgba(255, 255, 255, 0.7)",
+			"white-600": "rgba(255, 255, 255, 0.6)",
+			"white-500": "rgba(255, 255, 255, 0.5)",
+			"white-400": "rgba(255, 255, 255, 0.4)",
+			"white-300": "rgba(255, 255, 255, 0.3)",
+			"white-200": "rgba(255, 255, 255, 0.2)",
+			"white-100": "rgba(255, 255, 255, 0.1)"
+		}
 	},
 	plugins: []
 };
