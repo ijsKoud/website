055d8d96a7e0eabb5cd8e7dbe2f78fa050d0bb06
ijsKoud <github@ijskoud.dev>
2022-11-02 22:32:26 +0100

    feat(Home): add landing section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-	return <div className="h-[200vh]">Home</div>;
+	return (
+		<div className="grid place-items-center min-h-screen -mt-[70px]">
+			<div>
+				<h3 className="text-xl text-primary font-normal leading-3">HEY, MY NAME IS</h3>
+				<h1 className="text-4xl font-extrabold">DAAN KLARENBEEK</h1>
+				<h2 className="text-2xl text-dark-gray dark:text-light-gray font-semibold leading-10 w-[42rem] -mt-3">
+					Full-stack developer building apps for the internet.
+				</h2>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
