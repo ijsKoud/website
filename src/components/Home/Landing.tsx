a587cde5235650d6b513c777f85732d198311795
ijsKoud <github@ijskoud.dev>
2022-11-03 21:41:44 +0100

    fix(Home): responsiveness for smaller devices

--- src/components/Home/Landing.tsx
+++ src/components/Home/Landing.tsx
@@ -5,14 +5,14 @@ const Landing = () => {
 	return (
 		<div className="flex flex-col justify-center items-center min-h-screen">
 			<div className="flex flex-col">
-				<h3 className="text-xl text-primary font-normal leading-3 max-lg:text-lg max-md:text-base">HEY, MY NAME IS</h3>
-				<h1 className="text-4xl font-extrabold max-lg:text-3xl max-md:text-2xl max-sm:text-xl">DAAN KLARENBEEK</h1>
-				<h2 className="text-2xl text-dark-gray dark:text-light-gray font-semibold leading-10 w-[42rem] -mt-3 max-lg:text-xl max-lg:w-[34rem] max-md:text-lg max-md:leading-8 max-md:w-[26rem] max-sm:text-base max-sm:w-[22rem] max-sm:leading-6">
+				<h3 className="text-xl text-primary font-normal leading-3 max-lg:text-lg max-md:text-base max-sm:text-sm">HEY, MY NAME IS</h3>
+				<h1 className="text-4xl font-extrabold max-lg:text-3xl max-md:text-2xl max-sm:text-lg max-sm:leading-7">DAAN KLARENBEEK</h1>
+				<h2 className="text-2xl text-dark-gray dark:text-light-gray font-semibold leading-10 w-[42rem] -mt-3 max-lg:text-xl max-lg:w-[34rem] max-md:text-lg max-md:leading-8 max-md:w-[26rem] max-sm:text-sm max-sm:w-[15rem] max-sm:leading-4 max-sm:mt-px">
 					Full-stack developer building apps for the internet.
 				</h2>
 				<Link
 					href="#about"
-					className="text-base font-semibold text-primary border-2 border-primary px-4 py-2 rounded-md w-fit mt-6 hover:bg-primary-300 hover:text-white transition-colors"
+					className="text-base max-sm:text-sm max-sm:px-2 max-sm:py-1 font-semibold text-primary border-2 border-primary px-4 py-2 rounded-md w-fit mt-6 hover:bg-primary-300 hover:text-white transition-colors"
 				>
 					About myself
 				</Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Landing from "../components/Home/Landing";
 
 const Home = () => {
 	return (
-		<div className="min-h-screen px-40 -mt-24">
+		<div className="min-h-screen px-40 -mt-24 max-lg:px-28 max-md:px-9 max-sm:px-3">
 			<Landing />
 			<About />
 		</div>
