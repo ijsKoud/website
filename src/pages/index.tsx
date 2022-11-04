6871f9f6c552d03c0be90551b4ed5af73501f1c1
Daan Klarenbeek <github@ijskoud.dev>
2022-11-04 16:10:37 +0100

    feat: add projects list (#63)
    * feat: add projects list
    
    * fix(Projects): css bug fixes
    
    * chore: add project data
    
    * fix(Projects): incorrect href id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "../components/Home/About";
 import Landing from "../components/Home/Landing";
+import Projects from "../components/Home/Projects";
 
 const Home = () => {
 	return (
 		<div className="min-h-screen px-40 -mt-24 max-lg:px-28 max-md:px-9 max-sm:px-3">
 			<Landing />
 			<About />
+			<Projects />
 		</div>
 	);
 };
