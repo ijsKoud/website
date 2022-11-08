2ca98b551581eb80c17c090c9c46b0e28a47c7d6
ijsKoud <github@ijskoud.dev>
2022-11-08 10:33:48 +0100

    feat: add HomeSectionLayout

---
+++ src/components/HomeSectionLayout.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Props {
+	title: string;
+	id: string;
+	width: string;
+}
+
+const HomeSectionLayout = ({ children, title, id, width }: React.PropsWithChildren<Props>) => {
+	return (
+		<div className="flex flex-col justify-center items-center min-h-screen mb-48">
+			<div className={`flex flex-col ${width} max-xl:w-11/12`}>
+				<div className="flex flex-col w-full">
+					<h3 id={id} className="text-xl font-extrabold max-sm:text-lg">
+						{title}
+					</h3>
+					<i className="border-black-500 dark:border-white-300 border mt-1" />
+				</div>
+				<div className="flex flex-col gap-32 max-lg:gap-12">{children}</div>
+			</div>
+		</div>
+	);
+};
+
+export default HomeSectionLayout;
