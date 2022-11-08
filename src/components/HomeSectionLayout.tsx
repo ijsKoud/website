44dbdec3d335fb5af5e568a57c0a8fba75592860
ijsKoud <github@ijskoud.dev>
2022-11-08 10:41:12 +0100

    feat(HomeSectionLayout): add contactTitle styling

--- src/components/HomeSectionLayout.tsx
+++ src/components/HomeSectionLayout.tsx
@@ -4,18 +4,27 @@ interface Props {
 	title: string;
 	id: string;
 	width: string;
+	contactTitle?: boolean;
 }
 
-const HomeSectionLayout = ({ children, title, id, width }: React.PropsWithChildren<Props>) => {
+const HomeSectionLayout = ({ children, title, id, width, contactTitle }: React.PropsWithChildren<Props>) => {
 	return (
 		<div className="flex flex-col justify-center items-center min-h-screen mb-48">
 			<div className={`flex flex-col ${width} max-xl:w-11/12`}>
-				<div className="flex flex-col w-full">
-					<h3 id={id} className="text-xl font-extrabold max-sm:text-lg">
-						{title}
-					</h3>
-					<i className="border-black-500 dark:border-white-300 border mt-1" />
-				</div>
+				{contactTitle ? (
+					<div className="flex flex-col items-center w-full">
+						<h3 id={id} className="text-2xl font-extrabold max-sm:text-lg">
+							{title}
+						</h3>
+					</div>
+				) : (
+					<div className="flex flex-col w-full">
+						<h3 id={id} className="text-xl font-extrabold max-sm:text-lg">
+							{title}
+						</h3>
+						<i className="border-black-500 dark:border-white-300 border mt-1" />
+					</div>
+				)}
 				{children}
 			</div>
 		</div>
