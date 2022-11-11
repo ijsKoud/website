f291e4bc3896538dfa50d4f5bed9ae11e6a56b60
ijsKoud <github@ijskoud.dev>
2022-11-11 19:16:57 +0100

    feat(Navbar): add animation to link items

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,8 +2,11 @@ import Link from "next/link";
 import React from "react";
 import Menu from "./Menu";
 import ThemeButton from "./ThemeButton";
+import { motion } from "framer-motion";
+import { NavbarItemAnimations, NavbarTitleAnimations } from "../animations/Navbar";
 
 const Navbar = () => {
+	const navItemClassName = "text-base font-semibold hover:text-primary transition-colors cursor-pointer";
 	const scrollTop = () => {
 		const el = document.getElementById("content");
 		el?.scrollTo({ top: 0 });
@@ -11,19 +14,33 @@ const Navbar = () => {
 
 	return (
 		<div className="px-12 max-md:px-8 h-[70px] w-screen fixed z-[9999] flex justify-between items-center">
-			<Link href="/" onClick={scrollTop} className="text-xl font-extrabold w-fit hover:text-primary transition-colors">
-				ijsKoud
+			<Link href="/" legacyBehavior>
+				<motion.a
+					variants={NavbarTitleAnimations}
+					initial="initial"
+					animate="animate"
+					onClick={scrollTop}
+					className="text-xl font-extrabold w-fit hover:text-primary transition-colors cursor-pointer"
+				>
+					ijsKoud
+				</motion.a>
 			</Link>
 			<div className="flex justify-center gap-12 items-center max-md:gap-8">
 				<div className="flex justify-between gap-12 items-center max-md:hidden">
-					<Link href="#about" className="text-base font-semibold hover:text-primary transition-colors">
-						About
+					<Link legacyBehavior href="#about">
+						<motion.a variants={NavbarItemAnimations(0)} initial="initial" animate="animate" className={navItemClassName}>
+							About
+						</motion.a>
 					</Link>
-					<Link href="#projects" className="text-base font-semibold hover:text-primary transition-colors">
-						Projects
+					<Link legacyBehavior href="#projects">
+						<motion.a variants={NavbarItemAnimations(1)} initial="initial" animate="animate" className={navItemClassName}>
+							Projects
+						</motion.a>
 					</Link>
-					<Link href="#contact" className="text-base font-semibold hover:text-primary transition-colors">
-						Contact
+					<Link legacyBehavior href="#contact">
+						<motion.a variants={NavbarItemAnimations(2)} initial="initial" animate="animate" className={navItemClassName}>
+							Contact
+						</motion.a>
 					</Link>
 				</div>
 				<ThemeButton />
