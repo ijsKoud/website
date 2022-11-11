a6ff021d11e382ab51fd4c7c93b78fe20a7efc79
ijsKoud <github@ijskoud.dev>
2022-11-11 21:03:20 +0100

    feat(HomeSectionLayout): add fade-in animation

--- src/components/HomeSectionLayout.tsx
+++ src/components/HomeSectionLayout.tsx
@@ -1,3 +1,4 @@
+import { Variants, motion } from "framer-motion";
 import React from "react";
 
 interface Props {
@@ -7,9 +8,30 @@ interface Props {
 	contactTitle?: boolean;
 }
 
-const HomeSectionLayout = ({ children, title, id, width, contactTitle }: React.PropsWithChildren<Props>) => {
+const LayoutAnimation: Variants = {
+	initial: {
+		opacity: 0,
+		transform: "translateY(1rem)"
+	},
+	inView: {
+		opacity: 1,
+		transform: "translateY(0rem)",
+		transition: {
+			duration: 0.5,
+			ease: [0.6, 0, 0.17, 1]
+		}
+	}
+};
+
+const HomeSectionLayout: React.FC<React.PropsWithChildren<Props>> = ({ children, title, id, width, contactTitle }) => {
 	return (
-		<div className="flex flex-col justify-center items-center min-h-screen mb-48">
+		<motion.div
+			variants={LayoutAnimation}
+			initial="initial"
+			whileInView="inView"
+			viewport={{ once: true, amount: 0.5 }}
+			className="flex flex-col justify-center items-center min-h-screen mb-48"
+		>
 			<div className={`flex flex-col ${width} max-xl:w-11/12`}>
 				{contactTitle ? (
 					<div className="flex flex-col items-center w-full">
@@ -27,7 +49,7 @@ const HomeSectionLayout = ({ children, title, id, width, contactTitle }: React.P
 				)}
 				{children}
 			</div>
-		</div>
+		</motion.div>
 	);
 };
 
