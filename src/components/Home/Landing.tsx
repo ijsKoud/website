5e741f899c17f8fd26d2cef0b0251e1766254046
Daan Klarenbeek <github@ijskoud.dev>
2022-11-12 11:50:17 +0100

    feat: add loading animations (#68)
    * feat: add page transition progress-bar
    
    * feat: add page transition

--- src/components/Home/Landing.tsx
+++ src/components/Home/Landing.tsx
@@ -10,12 +10,12 @@ const Landing = () => {
 				<div className="block-reveal block-reveal-tertairy">
 					<h3 className="text-xl text-primary font-normal max-lg:text-lg max-md:text-base max-sm:text-sm w-fit">HEY, MY NAME IS</h3>
 				</div>
-				<div className="block-reveal block-reveal-delay-1">
+				<div className="block-reveal block-reveal-name block-reveal-delay-1">
 					<h1 className="text-4xl leading-tight font-extrabold max-lg:text-3xl max-md:text-2xl max-sm:text-lg max-sm:leading-7">
 						DAAN KLARENBEEK
 					</h1>
 				</div>
-				<div className="block-reveal block-reveal-light-gray block-reveal-delay-2 pt-2">
+				<div className="block-reveal block-reveal-slogan block-reveal-delay-2 pt-2">
 					<h2 className="text-2xl text-dark-gray dark:text-light-gray font-semibold leading-10 w-[42rem] -mt-3 max-lg:text-xl max-lg:w-[34rem] max-md:text-lg max-md:leading-8 max-md:w-[26rem] max-sm:text-sm max-sm:w-[15rem] max-sm:leading-4 max-sm:mt-px">
 						Full-stack developer building apps for the internet.
 					</h2>
@@ -23,7 +23,7 @@ const Landing = () => {
 				<Link href="#about" legacyBehavior>
 					<motion.a
 						variants={LandingButtonAnimation}
-						className="text-base max-sm:text-sm max-sm:px-2 max-sm:py-1 font-semibold text-primary border-2 border-primary px-4 py-2 rounded-md w-fit mt-6 hover:bg-primary-300 hover:dark:text-white hover:text-black transition-colors"
+						className="text-base cursor-pointer max-sm:text-sm max-sm:px-2 max-sm:py-1 font-semibold text-primary border-2 border-primary px-4 py-2 rounded-md w-fit mt-6 hover:bg-primary-300 hover:dark:text-white hover:text-black transition-colors"
 					>
 						About myself
 					</motion.a>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,40 @@
 import "../styles/globals.css";
+import "nprogress/nprogress.css";
 
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import NProgress from "nprogress";
 import Layout from "../components/Layout";
 import Cursor from "../components/Cursor";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const App = ({ Component, pageProps }: AppProps) => {
+	const { route, events } = useRouter();
+	useEffect(() => {
+		const handleRouteStart = () => NProgress.start();
+		const handleRouteDone = () => NProgress.done();
+
+		events.on("routeChangeStart", handleRouteStart);
+		events.on("routeChangeComplete", handleRouteDone);
+		events.on("routeChangeError", handleRouteDone);
+
+		return () => {
+			events.off("routeChangeStart", handleRouteStart);
+			events.off("routeChangeComplete", handleRouteDone);
+			events.off("routeChangeError", handleRouteDone);
+		};
+	}, []);
+
 	return (
 		<ThemeProvider enableSystem enableColorScheme attribute="class">
 			<Layout>
-				<Component {...pageProps} />
+				<AnimatePresence mode="wait">
+					<motion.div key={route} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+						<Component {...pageProps} />
+					</motion.div>
+				</AnimatePresence>
 			</Layout>
 			<Cursor />
 		</ThemeProvider>
