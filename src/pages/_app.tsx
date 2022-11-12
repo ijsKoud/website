b82caa77fc3dd0807d4c61ff77e4242c7b09388c
ijsKoud <github@ijskoud.dev>
2022-11-12 11:24:04 +0100

    feat: add page transition progress-bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,37 @@
 import "../styles/globals.css";
+import "nprogress/nprogress.css";
 
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import NProgress from "nprogress";
 import Layout from "../components/Layout";
 import Cursor from "../components/Cursor";
+import { Router } from "next/router";
+import { useEffect } from "react";
+import { AnimatePresence } from "framer-motion";
 
 const App = ({ Component, pageProps }: AppProps) => {
+	useEffect(() => {
+		const handleRouteStart = () => NProgress.start();
+		const handleRouteDone = () => NProgress.done();
+
+		Router.events.on("routeChangeStart", handleRouteStart);
+		Router.events.on("routeChangeComplete", handleRouteDone);
+		Router.events.on("routeChangeError", handleRouteDone);
+
+		return () => {
+			Router.events.off("routeChangeStart", handleRouteStart);
+			Router.events.off("routeChangeComplete", handleRouteDone);
+			Router.events.off("routeChangeError", handleRouteDone);
+		};
+	}, []);
+
 	return (
 		<ThemeProvider enableSystem enableColorScheme attribute="class">
 			<Layout>
-				<Component {...pageProps} />
+				<AnimatePresence>
+					<Component {...pageProps} />
+				</AnimatePresence>
 			</Layout>
 			<Cursor />
 		</ThemeProvider>
