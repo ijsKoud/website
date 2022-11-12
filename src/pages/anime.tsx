9a4285f86585e53118219760d2f08f992c91c7fb
ijsKoud <github@ijskoud.dev>
2022-11-12 11:50:42 +0100

    feat: add anime page

---
+++ src/pages/anime.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeSectionLayout from "../components/HomeSectionLayout";
+
+const Anime = () => {
+	return (
+		<div className="min-h-screen px-40 -mt-24 max-lg:px-28 max-md:px-9 max-sm:px-3 place-items-center grid">
+			<HomeSectionLayout title="ANIME" id="anime" width="w-4/5"></HomeSectionLayout>
+		</div>
+	);
+};
+
+export default Anime;
