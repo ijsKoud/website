2307d24a00b5dc5ea81141bbe44a23afb9b94200
ijsKoud <github@ijskoud.dev>
2022-11-13 21:38:16 +0100

    feat: add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import Cursor from "../components/Cursor";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
 	const { route, events } = useRouter();
@@ -29,6 +30,22 @@ const App = ({ Component, pageProps }: AppProps) => {
 
 	return (
 		<ThemeProvider enableSystem enableColorScheme attribute="class">
+			<Head>
+				<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+				<link rel="manifest" href="/favicons/site.webmanifest" />
+				<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#4E9ED7" />
+				<link rel="shortcut icon" href="/favicons/favicon.ico" />
+				<meta name="msapplication-TileColor" content="#4E9ED7" />
+				<meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+				<meta name="theme-color" content="#4E9ED7" />
+
+				<meta property="og:title" content="Daan Klarenbeek" />
+				<meta property="og:type" content="site" />
+				<meta property="og:description" content="Full-stack developer building apps for the internet." />
+			</Head>
 			<Layout>
 				<AnimatePresence mode="wait">
 					<motion.div key={route} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
