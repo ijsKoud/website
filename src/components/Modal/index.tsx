c1ce33cd3c205afa6e8f94a76d07bf916891a8f7
ijsKoud <github@ijskoud.dev>
2022-11-13 20:10:42 +0100

    feat(Modal): add screen-reader stuff

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,5 +1,5 @@
 import { AnimatePresence } from "framer-motion";
-import React from "react";
+import React, { useEffect } from "react";
 import ModalItem from "./ModalItem";
 import ReactDOM from "react-dom";
 
@@ -11,6 +11,11 @@ interface Props {
 const Modal: React.FC<React.PropsWithChildren<Props>> = ({ children, onClick, isOpen }) => {
 	const BodyEl = document?.getElementById("__next");
 	const ChildEl = <AnimatePresence mode="wait">{isOpen && <ModalItem onClick={onClick}>{children}</ModalItem>}</AnimatePresence>;
+
+	useEffect(() => {
+		if (isOpen && BodyEl) BodyEl.ariaHidden = "true";
+		else if (BodyEl) BodyEl.ariaHidden = "false";
+	}, [isOpen]);
 
 	return BodyEl ? ReactDOM.createPortal(ChildEl, BodyEl) : <div></div>;
 };
