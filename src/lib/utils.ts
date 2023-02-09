3b023ff920fcac1484d026dc385e1bf5e58d07e9
Daan Klarenbeek <github@ijskoud.dev>
2023-02-09 14:51:03 +0100

    fix(Age): incorrect result

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 export const getAge = (): number => {
 	const bday = Date.parse(new Date("12 February 2006").toString());
 	const current = Date.now();
-	const sumOfTime = 365 * 60 * 60 * 24 * 1e3;
+	const sumOfTime = 3.156e10;
 
 	return (current - bday) / sumOfTime;
 };
