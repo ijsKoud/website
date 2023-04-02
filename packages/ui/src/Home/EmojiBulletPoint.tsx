5b77b698bbfdb18848a0f607cddccfe1da8a4b16
ijsKoud <github@ijskoud.dev>
2023-04-02 20:23:21 +0200

    feat: make website responsive

--- packages/ui/src/Home/EmojiBulletPoint.tsx
+++ packages/ui/src/Home/EmojiBulletPoint.tsx
@@ -10,9 +10,9 @@ export const EmojiBulletPoint: React.FC<Props> = ({ text, emoji }) => {
 	const twemoji = parse(emoji, { assetType: "svg" })[0];
 
 	return (
-		<div className="flex items-center gap-5">
-			<img className="h-10" alt={emoji} src={twemoji.url} />
-			<h4 className="text-10 text-light-gray">{text}</h4>
+		<div className="flex items-center gap-5 mt-4 max-md:gap-2">
+			<img className="h-10 max-lg:h-8 max-md:h-6" alt={emoji} src={twemoji.url} />
+			<h4 className="text-10 text-light-gray max-lg:text-8 max-sm:text-6 max-[400px]:text-4 leading-[95%]">{text}</h4>
 		</div>
 	);
 };
