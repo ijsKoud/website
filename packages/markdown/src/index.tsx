ad12a09e7b6c0ba14449fdf9cad07f4d7792f445
ijsKoud <github@ijskoud.dev>
2023-04-04 20:39:01 +0200

    refactor(Markdown): add more props

--- packages/markdown/src/index.tsx
+++ packages/markdown/src/index.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import type { ReactMarkdownOptions } from "react-markdown/lib/react-markdown";
 
 interface Props {
 	children: string;
 }
 
-const Markdown: React.FC<Props> = ({ children }) => {
+const Markdown: React.FC<Props & ReactMarkdownOptions> = ({ children, ...props }) => {
 	return (
 		<ReactMarkdown
 			skipHtml
-			allowedElements={["strong", "code", "p"]}
 			components={{
 				strong: ({ children }) => <span className="dark:text-white text-[#000]">{children}</span>,
-				code: ({ children }) => <span className="text-primary">{children}</span>
+				code: ({ children }) => <span className="text-primary">{children}</span>,
+				a: ({ children, node, ...props }) => (
+					<a {...props} className="text-primary hover:text-primary-500 transition-colors">
+						{children}
+					</a>
+				)
 			}}
+			{...props}
 		>
 			{children}
 		</ReactMarkdown>
