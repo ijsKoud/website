ebb2ef1457a67674189482feedbb06350436e314
ijsKoud <github@ijskoud.dev>
2023-04-05 10:49:57 +0200

    refactor(Home): clean-up & use components

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,32 +1,19 @@
 import { ABOUT_ME_EXPERIENCE, ABOUT_ME_TEXT, LANDING_BULLET_POINTS, LANDING_TEXT } from "@website/constants";
-import { EmojiBulletPoint, PageLayout, SlideFade } from "@website/ui";
+import { EmojiBulletPoint, PageLayout, PageSection, SlideFade, GridIllustration, Experience } from "@website/ui";
 import { BlackButton, PrimaryButtonArrow } from "@website/buttons";
 import type { NextPage } from "next";
 import { Inter } from "next/font/google";
 import Markdown from "@website/markdown";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"], display: "swap", weight: ["800", "600"] });
 
 const Home: NextPage = () => {
-	const [theme, setTheme] = useState("dark");
-	const { theme: _theme } = useTheme();
-
-	useEffect(() => {
-		if (_theme) setTheme(_theme);
-	}, [_theme]);
-
 	return (
 		<PageLayout className="min-h-screen flex flex-col gap-y-52">
-			<section className="flex flex-col justify-center h-[calc(100vh-192px)] pe-[10vw] ps-[10vw] max-md:pe-[5vw] max-md:ps-[5vw] max-sm:pe-0 max-sm:ps-0">
+			{/* <-- LANDING INTRODUCTION SECTION --> */}
+			<PageSection className="h-[calc(100vh-192px)]">
 				<SlideFade delay={0.5} className="relative flex flex-col gap-y-1">
-					<img
-						aria-hidden
-						src={theme === "light" ? "/grid/light.svg" : "/grid/dark.svg"}
-						alt="Grid illustration"
-						className="absolute -z-10 top-0 left-0 -translate-x-11 -translate-y-11"
-					/>
+					<GridIllustration className="top-0 left-0 -translate-x-11 -translate-y-11" />
 					<h2 className="text-primary text-12 font-normal leading-[95%] w-fit max-lg:text-10 max-sm:text-8 max-[400px]:text-6">
 						<Markdown allowedElements={["strong", "code", "p"]}>{LANDING_TEXT.title.toUpperCase()}</Markdown>
 					</h2>
@@ -48,19 +35,16 @@ const Home: NextPage = () => {
 					<PrimaryButtonArrow type="link" href="/#about" className="max-[400px]:text-4">
 						About myself
 					</PrimaryButtonArrow>
-					<BlackButton type="link" href="/#about" className="max-[400px]:text-4">
+					<BlackButton type="link" href="/guestbook" className="max-[400px]:text-4">
 						Guest book
 					</BlackButton>
 				</SlideFade>
-			</section>
-			<section className="flex flex-col items-center justify-center pe-[10vw] ps-[10vw] max-md:pe-[5vw] max-md:ps-[5vw] max-sm:pe-0 max-sm:ps-0">
+			</PageSection>
+
+			{/* <-- ABOUT ME SECTION --> */}
+			<PageSection className="items-center">
 				<div className="flex flex-col gap-y-4 relative">
-					<img
-						aria-hidden
-						src={theme === "light" ? "/grid/light.svg" : "/grid/dark.svg"}
-						alt="Grid illustration"
-						className="absolute -z-10 top-0 left-0 -translate-x-11 -translate-y-11 h-28"
-					/>
+					<GridIllustration className="top-0 left-0 -translate-x-11 -translate-y-11 h-28" />
 					<h1 id="about" className="text-16 leading-[80%] w-fit max-lg:text-14 max-sm:text-12 max-[400px]:text-10" style={inter.style}>
 						About Me
 					</h1>
@@ -76,22 +60,11 @@ const Home: NextPage = () => {
 					</div>
 					<div className="flex flex-col gap-y-4">
 						{Object.keys(ABOUT_ME_EXPERIENCE).map((id, key) => (
-							<div key={key}>
-								<h2 className="text-6" style={inter.style}>
-									{id}
-								</h2>
-								<ul>
-									{ABOUT_ME_EXPERIENCE[id as keyof typeof ABOUT_ME_EXPERIENCE].map((item, k) => (
-										<li key={k} className="float-left mx-1 first:ml-0 last:mr-0">
-											<img alt={item.name} className="h-14" src={item.image} />
-										</li>
-									))}
-								</ul>
-							</div>
+							<Experience key={key} id={id} style={inter.style} />
 						))}
 					</div>
 				</div>
-			</section>
+			</PageSection>
 		</PageLayout>
 	);
 };
