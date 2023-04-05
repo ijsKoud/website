cec47df34c81dd0359bca1b0349c40516e29a44d
ijsKoud <github@ijskoud.dev>
2023-04-05 11:17:54 +0200

    feat(Buttons): add smooth scroll

--- packages/buttons/src/Button.tsx
+++ packages/buttons/src/Button.tsx
@@ -10,8 +10,25 @@ const Button: React.FC<React.PropsWithChildren<ButtonProps>> = (props) => {
 			</button>
 		);
 
+	const onClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+		props.onClick?.(event);
+
+		const href = props.href.toString().replace("/", "");
+		if (href.startsWith("#")) {
+			event.preventDefault();
+
+			const destination = document.querySelector(href);
+			if (destination) {
+				location.href = props.href.toString();
+				destination.scrollIntoView({
+					behavior: "smooth"
+				});
+			}
+		}
+	};
+
 	return (
-		<Link {...props} className={`w-fit ${props.className ?? ""}`}>
+		<Link {...props} onClick={onClick} className={`w-fit ${props.className ?? ""}`}>
 			{props.children}
 		</Link>
 	);
