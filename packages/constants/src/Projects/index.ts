6039930afffaaea4396d221bb1ede2e9bc57e9e6
ijsKoud <github@ijskoud.dev>
2023-04-06 10:27:04 +0200

    feat: add project card

--- packages/constants/src/Projects/index.ts
+++ packages/constants/src/Projects/index.ts
@@ -4,14 +4,37 @@ export const PROJECTS_LIST = [
 		name: "PaperPlane",
 		description:
 			"PaperPlane is a file-upload server build for the [ShareX](https://getsharex.com) screenshot capture application. With [docker](https://docker.io) support you can easily install it on your favourite device, shipped with clean minimalistic dashboard, front-end uploading, password protections for files and pastebin support and a lot more.",
-		badges: ["React"]
-	}
-] as const;
-
-export const PROJECT_BADGES = [
+		urls: [
+			{
+				url: "/github/paperplane",
+				icon: "fa-brands fa-github"
+			}
+		]
+	},
 	{
-		name: "React",
-		icon: "",
-		color: "#47c4ea"
+		image: "https://cdn.ijskoud.dev/files/UAVrJ9t0CRhz.png",
+		name: "SCR Creator Hub",
+		description: "Provider of stock footage and images for Stepford County Railway content creators.",
+		urls: [
+			{
+				url: "https://scrcreate.app/",
+				icon: "fa-solid fa-arrow-up-right-from-square"
+			}
+		]
+	},
+	{
+		image: "https://cdn.ijskoud.dev/files/xaTEOXlBgo9i.png",
+		name: "Website",
+		description: "This portfolio built with Next.js, Tailwind and TypeScript. Hosted on Vercel.",
+		urls: [
+			{
+				url: "/github/website",
+				icon: "fa-brands fa-github"
+			},
+			{
+				url: "https://ijskoud.dev/",
+				icon: "fa-solid fa-arrow-up-right-from-square"
+			}
+		]
 	}
-] as const;
+];
