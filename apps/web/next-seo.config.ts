16b6f6a836cc3701f280251d09fb74641da71e4c
ijsKoud <github@ijskoud.dev>
2023-04-10 11:16:46 +0200

    feat(Web): add next-seo

---
+++ apps/web/next-seo.config.ts
@@ -0,0 +1,18 @@
+import type { DefaultSeoProps } from "next-seo";
+type SeoProps = Omit<DefaultSeoProps, "children">;
+
+const config: SeoProps = {
+	titleTemplate: "Daan Klarenbeek • %s",
+	defaultTitle: "Daan Klarenbeek • Full-stack developer building apps for the internet",
+	themeColor: "#50a0d4",
+	dangerouslySetAllPagesToNoFollow: false,
+	dangerouslySetAllPagesToNoIndex: false,
+	description: "Full-stack developer building apps for the internet",
+	twitter: { cardType: "summary" },
+	openGraph: {
+		description: "Full-stack developer building apps for the internet",
+		images: [{ url: "/profile.png", alt: "Profile picture", type: "image/png" }]
+	}
+};
+
+export default config;
