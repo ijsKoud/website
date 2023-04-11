390a9a5916ca6bf9242b0f2fac60d04002cf77bb
ijsKoud <github@ijskoud.dev>
2023-04-11 21:43:16 +0200

    feat(Constants): add anime constants

---
+++ packages/constants/src/Anime/index.ts
@@ -0,0 +1,46 @@
+import type { Variants } from "framer-motion";
+
+export interface AnimeAPIResponse {
+	list: AnimeAPIListEntry[];
+	username: string;
+}
+
+export interface AnimeAPIListEntry {
+	title: string;
+	title_english: string;
+	genres: string[];
+	rating: number;
+	status: number;
+	episodes: AnimeAPIEpisodes;
+	image: string;
+	url: string;
+	banner: string;
+}
+
+export interface AnimeAPIEpisodes {
+	watched: number;
+	total: number;
+}
+
+export const ANIME_API_URL = "https://api.ijskoud.dev/anime/stats";
+
+/**
+ * Returns a FadeIn animation with a delay of `0.5 * key`
+ * @param key The position of the item in the array
+ * @returns Framer-motion variants
+ */
+export const FadeInAnimationKey = (key: number): Variants => ({
+	initial: {
+		opacity: 0,
+		transform: "translateY(1rem)"
+	},
+	inView: {
+		opacity: 1,
+		transform: "translateY(0rem)",
+		transition: {
+			duration: 0.5,
+			delay: 0.05 * key,
+			ease: [0.6, 0, 0.17, 1]
+		}
+	}
+});
--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -2,3 +2,4 @@ export * from "./Navbar";
 export * from "./Home";
 export * from "./Animations";
 export * from "./Projects";
+export * from "./Anime";
